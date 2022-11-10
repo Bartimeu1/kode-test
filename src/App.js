f67775fe130c5523cd4ef03d4f63990bde3338af
Ivan Sapego <sapego-ivan@list.ru>
2022-11-10 16:56:12 +0300

    fixed routers

--- src/App.js
+++ src/App.js
@@ -56,8 +56,31 @@ function App() {
   return (
     <div className="App">
       <Context.Provider value={contextItems}>
-
-        123123
+        <Routes>
+          <Route path="/" exact element={
+            <MainPage
+              choiceFilter={choiceFilter}
+              onInputSearch={onInputSearch}
+              currentContacts={currentContacts}
+            ></MainPage>
+          }>
+          </Route>
+          {contactsData.map(contact => { 
+            return <Route path={`/profile/${contact.userTag}`} key={contact}
+              element={
+                <ProfilePage 
+                  contactName={contact.firstName}
+                  contactSurname={contact.lastName}
+                  contactAvarar={contact.avatarUrl}
+                  contactTag={contact.userTag}
+                  contactPosition={contact.position}
+                  contactPhone={contact.phone}
+                  contactBirthday={contact.birthday}
+                ></ProfilePage>
+              }>
+           </Route>
+          })}
+        </Routes>
       </Context.Provider>
     </div>
   );
